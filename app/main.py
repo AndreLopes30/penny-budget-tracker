d4632d007c687839d74d4da708f9d1437c87ca50
AndreLopes30 <ferreiraandre833@gmail.com>
2026-03-11 19:06:49 -0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.api import api_router
+from app.db.base_class import Base
+from app.db.db import engine
+import logging
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    logging.info("Forçando a criação das tabelas no Banco de Dados...")
+    Base.metadata.create_all(bind=engine)
+    logging.info("Tabelas criadas com sucesso!")
 
 app.add_middleware(
     CORSMiddleware,
