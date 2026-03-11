8f0512ca14e1e242cca0b2eafa84728e84269e57
AndreLopes30 <ferreiraandre833@gmail.com>
2026-03-11 18:55:20 -0300

    feat: muda URL da API para producao no Render

--- app/db/db.py
+++ app/db/db.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.db.base_class import Base 
-from app.models.transaction import Transaction
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:@localhost:5432/Penny")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -16,10 +18,6 @@ def get_db():
     finally:
         db.close()
 
-def create_tables():
-    Base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-    print("⏳ Criando tabelas...")
-    create_tables()
-    print("✅ Sucesso!")
+print("⏳ Verificando/Criando tabelas no banco de dados...")
+Base.metadata.create_all(bind=engine)
+print("✅ Tabelas prontas!")
